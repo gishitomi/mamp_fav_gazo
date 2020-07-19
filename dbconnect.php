1f151beeab55087ad314f1d561ae2aa611f11afc
宜志富紹太 <shotagishitomi@gmail.com>
2020-07-19 23:35:49 +0900

    画像が表示されない問題が見つかったので、一時ローカルで動かすことにする 7/19

--- dbconnect.php
+++ dbconnect.php
@@ -1,15 +1,15 @@
 <?php
-// $dbname = 'fav_gazo';
-// $host = 'localhost';
-// $charset = 'utf8';
-// $username  = 'root';
-// $password = 'root';
+$dbname = 'fav_gazo';
+$host = 'localhost';
+$charset = 'utf8';
+$username  = 'root';
+$password = 'root';
 
-$host = getenv('HOSTNAME'); //MySQLがインストールされてるコンピュータ
-$dbname = getenv('DBNAME'); //使用するDB
-$charset = "utf8"; //文字コード
-$username = getenv('USERNAME'); //MySQLにログインするユーザー名
-$password = getenv('PASSWORD'); //ユーザーのパスワード
+// $host = getenv('HOSTNAME'); //MySQLがインストールされてるコンピュータ
+// $dbname = getenv('DBNAME'); //使用するDB
+// $charset = "utf8"; //文字コード
+// $username = getenv('USERNAME'); //MySQLにログインするユーザー名
+// $password = getenv('PASSWORD'); //ユーザーのパスワード
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //SQLでエラーが表示された場合、画面にエラーが出力される
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //DBから取得したデータを連想配列の形式で取得する
